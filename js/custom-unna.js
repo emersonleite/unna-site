ff2cd0ca079a45efd584739deab099756f2600fc
root <root@LAPTOP-8UOMEHFC.localdomain>
2019-05-15 15:15:05 +0000

    121415052019

--- js/custom-unna.js
+++ js/custom-unna.js
@@ -1,16 +1,18 @@
-var text = '<img src="/img/sacred-heart_250.png" width=100px> Sagrado </img>' ;
 
+
+var text = '<img src="/img/sacred-heart_250.png" width=75px></img>' ;
 var dateAtual = new Date;
-
 var elem = document.getElementById("msgSacre");
+var div1 = document.getElementById("div-footer");
+var elem1 = document.getElementById("text-author");
+var elem2 = document.getElementById("text-version");
+
 var day = dateAtual.getDate();
+var weekday = dateAtual.getDay();
 
-console.log(dateAtual);
-console.log(elem);
-console.log(day);
-
-
-if (day == 14){
+if (weekday == 5 | weekday == 2 ){
+    div1.removeChild(elem1);
+    div1.removeChild(elem2);
     elem.innerHTML = text;
 }
 
