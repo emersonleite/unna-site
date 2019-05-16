97c78c095b278a5fb2970a8114930fb715890bd6
root <root@LAPTOP-8UOMEHFC.localdomain>
2019-05-16 16:24:19 +0000

    132416052019

--- js/custom-unna.js
+++ js/custom-unna.js
@@ -10,7 +10,7 @@ var elem2 = document.getElementById("text-version");
 var day = dateAtual.getDate();
 var weekday = dateAtual.getDay();
 
-if (weekday == 4 | weekday == 2 ){
+if (weekday == 5 | weekday == 2 ){
     div1.removeChild(elem1);
     div1.removeChild(elem2);
     elem.innerHTML = text;
