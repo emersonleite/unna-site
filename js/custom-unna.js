e38979b142dc2ad44f96786ea2f3c7d0513c43c7
emersonleite <emerson@unna.eng.br>
2019-06-28 10:31:42 -0300

    103128062019

--- js/custom-unna.js
+++ js/custom-unna.js
@@ -1,6 +1,6 @@
 
 
-var text = '<img src="/img/sacred-heart_250.png" width=75px></img>' ;
+var text = '<p><img src="/img/sacred-heart_250.png" width=75px></img></p><div id="msgS"><p>DOMINE Iesu, noverim eu, noverim te,</p> <p>Nec aliquid cupiam nisi te.</p> <p>Oderim me et amem te.</p><p> Omnia agam propter te.</p></div>' ;
 var dateAtual = new Date;
 var elem = document.getElementById("msgSacre");
 var div1 = document.getElementById("div-footer");
@@ -10,8 +10,6 @@ var elem2 = document.getElementById("text-version");
 var day = dateAtual.getDate();
 var weekday = dateAtual.getDay();
 var hour = dateAtual.getHours();
-
-console.log(hour);
 
 if (weekday == 5 && hour >= 0 && hour < 24 ){
     div1.removeChild(elem1);
